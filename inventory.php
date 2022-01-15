2a68cafb75a58119010686d7aa11d228a3409110
mehdilagdimi <lagdimi.mehdi@gmail.com>
2022-01-15 23:17:30 +0100

    update invent page

--- inventory.php
+++ inventory.php
@@ -21,63 +21,67 @@ ini_set("display_errors", "stdout");
         <script src="https://kit.fontawesome.com/c4254e24a8.js"></script>
 </head>
 <body class="flex flex-row">  
-    <!-- <header> -->
-    <!-- Left bar -->
+        <!-- <header> -->
+            <!-- Left bar -->
 
-        <div class="left-bar-container bg-color center">
+            <div class="left-bar-container bg-color center">
 
-            <div><a href="#"><img class="logo" src="public\assets\images\Logo.png"></a></div>
-            <!-- Switch pages : stats /inventory -->
-            <div class="page-switcher">
-                <div id="invent-page">
-                    <h2><a href="#">Inventory</a></h2>
-                </div>
-                <div id="stats-page">
-                    <h2><a href="#">Stats</a></h2>
-                    <div id="border-left"></div>
-                </div>
+        <div><a href="#"><img class="logo" src="public\assets\images\Logo.png"></a></div>
+        <!-- Switch pages : stats /inventory -->
+        <div class="page-switcher">
+            <div id="invent-page">
+                <h2><a href="#">Inventory</a></h2>
             </div>
-            <!-- CRUD operations -->
-            <div class="flex flex-col center">
-                <div class="flex flex-row" >
-                    <div class="space-between"><img src="public/assets/images/add.svg"></div>
-                    <div class="space-between"><h1><a href="#">ADD</a></h1></div>
-                 </div>
-
-                 <div class="flex flex-row" >
-                    <div class="space-between"><img src="public/assets/images/update.svg"></div>
-                    <div class="space-between"><h1><a href="#">UPDATE</a></h1></div>
-                 </div>
-
-                 <div class="flex flex-row" >
-                    <div class="space-between"><img src="public/assets/images/delete.svg"></div>
-                    <div class="space-between"><h1><a href="#">DELETE</a></h1></div>
-                 </div>
+            <div id="stats-page">
+                <h2><a href="#">Stats</a></h2>
+                <div id="border-left"></div>
             </div>
+        </div>
+        <!-- CRUD operations -->
+        <div class="flex flex-col center">
+            <div class="flex flex-row" >
+                <div class="space-between"><img src="public/assets/images/add.svg"></div>
+                <div class="space-between"><h1><a href="#">ADD</a></h1></div>
+            </div>
+
+            <div class="flex flex-row" >
+                <div class="space-between"><img src="public/assets/images/update.svg"></div>
+                <div class="space-between"><h1><a href="#">UPDATE</a></h1></div>
+            </div>
+
+            <div class="flex flex-row" >
+                <div class="space-between"><img src="public/assets/images/delete.svg"></div>
+                <div class="space-between"><h1><a href="#">DELETE</a></h1></div>
+            </div>
+        </div>
 
         </div>
 
         <div class="main-page center">
-            <div class="flex flex-row center" style="width : inherit;">
-                <div class="search-container margin">
-                    <form for="search">
-                        <div class="input-icons">
-                            <i class="fas fa-search icon"></i>
-                            <input type="text" class="bg-color input-field" id="search" name="searchByName" placeholder="Search"> 
+        <div class="flex flex-row center" style="width : inherit;">
+            <div class="search-container margin">
+                <form action="" for="search">
+                    <div class="input-icons">
+                        <i class="fas fa-search icon"></i>
+                        <input type="text" class="bg-color input-field" id="search" name="searchByName" placeholder="Search"> 
+                    </div>
+                </form>
+            </div>
+            <div class="cat-container">
+                <form action="" for="cat">
+                    <div class="input-icons dropdown">
+                        <i class="fas fa-chevron-circle-down icon "></i>
+                        <div class="dropdown-content">
+                            <a href="#">Pixel Art</a>
+                            <a href="#">2D Graphics</a>
+                            <a href="#">3D Graphics</a>
                         </div>
-                    </form>
-                </div>
-                <div class="cat-container">
-                    <form for="cat">
-                        <div class="input-icons">
-                            <i class="fas fa-search icon"></i>
-                            <input type="text" class="bg-color input-field" id="cat" name="searchByCat" placeholder="All Categories"> 
-                        </div>
-                    </form>
-                </div>
+                        <input type="text" class="bg-color input-field" id="cat" name="searchByCat" placeholder="All Categories"> 
+                    </div>
+                </form>
             </div>
         </div>
-
+        </div>
     
 </body>
 </html>
