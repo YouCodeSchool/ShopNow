138ac2987626deaf2f6524a1c456d9a386ef16f9
mehdilagdimi <lagdimi.mehdi@gmail.com>
2022-01-18 11:01:04 +0100

    update invent page

--- inventory.php
+++ inventory.php
@@ -19,101 +19,115 @@ ini_set("display_errors", "stdout");
         <title>ShopNow</title>
         <link rel="stylesheet" href="public/css/inventory.css">
         <script src="https://kit.fontawesome.com/c4254e24a8.js"></script>
+        <script>
+            function Add_function(){
+
+            }
+            function Update_function(){
+                
+            }
+
+        </script>
 </head>
 <body class="flex flex-row">  
-        <!-- <header> -->
-            <!-- Left bar -->
+    <!-- <header> -->
+    <!-- Left bar -->
 
+        <div class="left-bar-container bg-color center">
 
-            <div class="left-bar-container bg-color center">
-
-                <div><a href="#"><img class="logo" src="public\assets\images\Logo.png"></a></div>
-                <!-- Switch pages : stats /inventory -->
-                <div class="page-switcher">
-                    <div class="bg-color" id="invent-page">
-                        <h2><a href="#">Inventory</a></h2>
-                    </div>
-                    <div id="stats-page">
-                        <h2><a href="#">Stats</a></h2>
-                        <div id="border-left"></div>
-                    </div>
+            <div><a href="#"><img class="logo" src="public\assets\images\Logo.png"></a></div>
+            <!-- Switch pages : stats /inventory -->
+            <div class="page-switcher">
+                <div class="bg-color" id="invent-page">
+                    <h2><a href="#">Inventory</a></h2>
                 </div>
-                <!-- CRUD operations -->
-                <div class="flex flex-col center">
-                    <div class="flex flex-row" >
-                        <div class="space-between_CRUD"><img src="public/assets/images/add.svg"></div>
-                        <div class="space-between_CRUD"><h1><a href="#">ADD</a></h1></div>
-                    </div>
-
-                    <div class="flex flex-row" >
-                        <div class="space-between_CRUD"><img src="public/assets/images/update.svg"></div>
-                        <div class="space-between_CRUD"><h1><a href="#">UPDATE</a></h1></div>
-                    </div>
-
-                    <div class="flex flex-row" >
-                        <div class="space-between_CRUD"><img src="public/assets/images/delete.svg"></div>
-                        <div class="space-between_CRUD"><h1><a href="#">DELETE</a></h1></div>
-                    </div>
+                <div id="stats-page">
+                    <h2><a href="#">Stats</a></h2>
+                    <div id="border-left"></div>
+                </div>
+            </div>
+            <!-- CRUD operations -->
+            <div class="flex flex-col center">
+                <div class="flex flex-row" onclick="Add_function()" >
+                    <div class="space-between_CRUD"><img src="public/assets/images/add.svg"></div>
+                    <div class="space-between_CRUD"><h1><a href="#">ADD</a></h1></div>
                  </div>
 
-                </div>
+                 <div class="flex flex-row" >
+                    <div class="space-between_CRUD"><img src="public/assets/images/update.svg"></div>
+                    <div class="space-between_CRUD"><h1><a href="#">UPDATE</a></h1></div>
+                 </div>
 
-                <div class="main-page center">
-                <div class="flex flex-row center margin" style="width : inherit;">
-                    <div class="search-container space-between">
-                        <form action="" for="search">
-                            <div class="input-icons">
-                                <i class="fas fa-search icon"></i>
-                                <input type="text" class="bg-color input-field" id="search" name="searchByName" placeholder="Search"> 
-                            </div>
-                        </form>
-                    </div>
-                    <div class="cat-container space-between">
-                        <form action="" for="cat">
-                            <div class="input-icons dropdown">
-                                <i class="fas fa-chevron-circle-down icon "></i>
-                                <div class="dropdown-content">
-                                    <a href="#">Pixel Art</a>
-                                    <a href="#">2D Graphics</a>
-                                    <a href="#">3D Graphics</a>
-                                </div>
-                                <input type="text" class="bg-color input-field" id="cat" name="searchByCat" placeholder="All Categories"> 
-                            </div>
-                        </form>
-                    </div>
+                 <div class="flex flex-row" >
+                    <div class="space-between_CRUD"><img src="public/assets/images/delete.svg"></div>
+                    <div class="space-between_CRUD"><h1><a href="#">DELETE</a></h1></div>
+                 </div>
+            </div>
+
+        </div>
+
+        <div class="main-page center">
+            <div class="flex flex-row center margin" style="width : inherit;">
+                <div class="search-container space-between">
+                    <form action="" for="search">
+                        <div class="input-icons">
+                            <i class="fas fa-search icon"></i>
+                            <input type="text" class="bg-color input-field" id="search" name="searchByName" placeholder="Search"> 
+                        </div>
+                    </form>
                 </div>
-                <!-- filter By section -->
-                <div class="flex flex-row center" style="margin-top : 0.8rem">
-                    <div class="flex flex-row center">
-                        <h2>In Stock</h2>
-                        <i class="fas fa-toggle-on space-between" style="font-size: 2rem;"></i>
-                    </div> 
+                <div class="cat-container space-between">
+                    <form action="" for="cat">
+                        <div class="input-icons dropdown">
+                            <i class="fas fa-chevron-circle-down icon "></i>
+                            <div class="dropdown-content">
+                                <a href="#">All Categories</a>
+                                <a href="#">Pixel Art</a>
+                                <a href="#">2D Graphics</a>
+                                <a href="#">3D Graphics</a>
+                            </div>
+                            <input type="text" class="bg-color input-field" id="cat" name="searchByCat" placeholder="All Categories"> 
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <!-- filter By section -->
+            <div class="flex flex-row center" style="margin-top : 0.8rem">
+                <div class="flex flex-row center">
+                    <h2>In Stock</h2>
+                    <i class="fas fa-toggle-on space-between" style="font-size: 2rem;"></i>
+                </div> 
+                <form class="flex flex-row center">
                     <div class="flex flex-row center">
                         <h2 >Price : MIN</h2>
-                        <form for="min-price">
+                            <!-- <label for="min-price"></label> -->
                             <input class="space-between color center" type="number" id="min-price" placeholder="0.0">
-                        </form>
+                        
                         <h2 >MAX</h2>
-                        <form for="max-price">
+                        <!-- <form for="max-price"> -->
                             <input class="space-between color center" type="number" id="max-price" placeholder="0.0">
-                        </form>
+                        <!-- </form> -->
                     </div>
                     <div class="flex flex-row center">
                         <h2 >From  </h2>
-                        <form for="start-date">
+                        <!-- <form for="start-date"> -->
                             <input class="space-between color center" type="date" id="start-date" placeholder="dd/jj/yyyy">
-                        </form>
+                        <!-- </form> -->
                         <h2 >To</h2>
-                        <form for="end-date">
+                        <!-- <form for="end-date"> -->
                             <input class="space-between color center" type="date" id="end-date" placeholder="">
-                        </form>
+                        <!-- </form> -->
                     </div>
-                </div>
+                    <div >
+                        <button class="bg-color" onclick="sendData()" type="submit">FILTER</button>
+                    </div>
+                </form>
+            </div>
 
-                <!-- Products tables/pages -->
-                <div class="flex center">
-                    <table style="table-layout: fixed; width : 100%;">
-                        <tr>
+            <!-- Products tables/pages -->
+            <div class="flex center">
+                <table style="table-layout: fixed; width : 100%;">
+                    <tr>
                         <th></th>
                         <th>#</th>
                         <th>Name</th>
@@ -178,21 +192,101 @@ ini_set("display_errors", "stdout");
                             <td><a href="#">Pixel</a></td>
                             <td><a href="#">00/00/2022</a></td>
                         </tr>
-                    </table>
-                </div>
+                  </table>
+            </div>
 
-                <!-- pagination -->
-                <div class="pagination">
-                    <a href="#">&laquo;</a>
-                    <a href="#">1</a>
-                    <a class="active" href="#">2</a>
-                    <a href="#">3</a>
-                    <a href="#">4</a>
-                    <a href="#">5</a>
-                    <a href="#">6</a>
-                    <a href="#">&raquo;</a>
-                </div>
-                </div>
+            <!-- pagination -->
+            <div class="pagination">
+                <a href="#">&laquo;</a>
+                <a href="#">1</a>
+                <a class="active" href="#">2</a>
+                <a href="#">3</a>
+                <a href="#">4</a>
+                <a href="#">5</a>
+                <a href="#">6</a>
+                <a href="#">&raquo;</a>
+              </div>
+
+              <!-- ADD PRODUCT BLOCK -->
+              <div class="ADD bg-color">
+                <form class="flex flex-col">
+                    <ul class="flex flex-row" style="justify-content: space-evenly;">
+                        <li>
+                            <label for="NFT_Name">NFT Name:</label>
+                            <input type="text" class="color" id="NFT_Name" name="NFT" placeholder="NFT Name"> 
+                        </li>
+                        <li>
+                            <label for="price">Price:</label>
+                            <input class="space-between center" type="number" id="price" placeholder="0.0">
+                        </li>
+                        <li>
+                            <label for="cat_add">Category:</label>
+                             <select class="color" id="cat_add" name="product_cat">
+                                <option value="pixel">Pixel</option>
+                                <option value="2D">2D</option>
+                                <option value="3D">3D</option>
+                            </select>
+                        </li>
+                       
+                        <li>
+                            <label for="start">Date of creation:</label>
+                            <input class="space-between color center" type="date" id="start" name="creation-date" value="2022-01-03" min="2022-01-03">
+                        </li>
+                        
+                    </ul>
+                    <ul>
+                        <li>
+                            <label for="NFT_src">NFT IMG Link:</label>
+                            <input type="text" style="width : 40%" class="color" id="NFT_src" name="NFT_img" placeholder="NFT link"> 
+                        </li>
+                        <li>
+                            <button type="submit" class="color" style="background-color: white; margin-top:0.5rem" onclick="sendData()">ADD</button>
+                        </li>
+                    </ul>
+
+                </form>
+
+              </div>
+              
+              <div class="UPDATE bg-color">
+                <form class="flex flex-col">
+                    <ul class="flex flex-row" style="justify-content: space-evenly;">
+                        <li>
+                            <label for="NFT_Name">NFT Name:</label>
+                            <input type="text" class="color" id="NFT_Name" name="NFT" placeholder="NFT Name"> 
+                        </li>
+                        <li>
+                            <label for="price">Price:</label>
+                            <input class="space-between center" type="number" id="price" placeholder="0.0">
+                        </li>
+                        <li>
+                            <label for="cat_add">Category:</label>
+                            <select class="color" id="cat_add" name="product_cat">
+                                <option value="pixel">Pixel</option>
+                                <option value="2D">2D</option>
+                                <option value="3D">3D</option>
+                            </select>
+                        </li>
+                    
+                        <li>
+                            <label for="start">Date of creation:</label>
+                            <input class="space-between color center" type="date" id="start" name="creation-date" value="2022-01-03" min="2022-01-03">
+                        </li>
+                        
+                    </ul>
+                    <ul>
+                        <li>
+                            <label for="NFT_src">NFT IMG Link:</label>
+                            <input type="text" style="width : 40%" class="color" id="NFT_src" name="NFT_img" placeholder="NFT link"> 
+                        </li>
+                        <li>
+                            <button type="submit" class="color" style="background-color: white; margin-top:0.5rem" onclick="sendData()">UPDATE</button>
+                        </li>
+                    </ul>
+
+                </form>
+            </div>
+        </div>
 
     
 </body>
